a3f3f45d3454b9c80a98db53a3aae23af5cba22b
Johannes Brodwall <jhannes@gmail.com>
2025-09-17 13:13:23 +0200

    install husky and prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import React from "react";
 
-createRoot(document.getElementById("app"))
-    .render(<h1>Hello React</h1>);
+createRoot(document.getElementById("app")).render(<h1>Hello React</h1>);
